52dcedd7953e3a4f42714fc8299f359025fd0a52
Helen mary varghese <65602897+helen579@users.noreply.github.com>
2021-06-21 00:34:19 +0530

    Update FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -1,12 +1,16 @@
-n = int(input("Enter the value of n: "))
 a = 0
 b = 1
 c = 0
 count = 1
-print("Fibonacci Series: ")
-while(count <= n):
-  print(c, end = " ")
-  count += 1
-  a = b
-  b = c
-  c = a + b
+
+n = int(input("Enter the value of n: "))
+if(n==0 or n==1):
+ print("Please enter value above 1!")
+else:
+    print("Fibonacci Series: ")
+    while(count <= n):
+      print(c, end = " ")
+      count += 1
+      a = b
+      b = c
+      c = a + b
